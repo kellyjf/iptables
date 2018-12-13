137dc88446ce876dbf6a63a897281f2814c6ca3a
John Kelly <jkelly@aircell.com>
2018-12-12 20:44:19 -0700

    Add addresses

--- demos/qos.sh
+++ demos/qos.sh
@@ -31,11 +31,11 @@ function linkns {
 rm -f /var/run/netns/main
 ln -s /proc/1/ns/net /var/run/netns/main
 
-for ns in router lap1 lap2; do
+for ns in router lan1 lan2; do
 	createns $ns
 done
 
-for ns in lap1 lap2; do
+for ns in lan1 lan2; do
 	linkns router $ns
 done
 
@@ -43,3 +43,22 @@ for ns in wan1 wan2; do
 	linkns router main $ns $ns
 done
 
+$NS main   ip addr add 10.10.100.1/30 dev wan1
+$NS main   ip addr add 10.10.200.1/30 dev wan2
+$NS router ip addr add 10.10.100.2/30 dev wan1
+$NS router ip addr add 10.10.200.2/30 dev wan2
+
+$NS router brctl addbr int-cabin
+$NS router ip link set int-cabin up
+$NS router brctl addif int-cabin lan1
+$NS router brctl addif int-cabin lan2
+$NS router ip addr add 10.10.0.1/24 dev int-cabin
+$NS router ip route add default via  10.10.100.1
+$NS router iptables -t nat -A POSTROUTING -o wan1 -j MASQUERADE
+$NS router iptables -t nat -A POSTROUTING -o wan2 -j MASQUERADE
+
+
+$NS lan1 ip addr add 10.10.0.100/24 dev router
+$NS lan1 ip route add default via 10.10.0.1
+$NS lan2 ip addr add 10.10.0.101/24 dev router
+$NS lan2 ip route add default via 10.10.0.1
